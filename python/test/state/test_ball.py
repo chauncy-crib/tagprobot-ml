2254238d893c9790147d53d2b53e5cf4660c7d58
David Abrahams <da.j.abrahams@gmail.com>
2019-08-31 14:36:15 -0700

    change variable names

--- python/test/state/test_ball.py
+++ python/test/state/test_ball.py
@@ -3,15 +3,14 @@ from hypothesis import given
 import hypothesis.strategies as st
 from hypothesis.searchstrategy.strategies import SearchStrategy
 
-
 from state.ball import Ball, Team
 
-# some random position between -1M and 1M
-ball_position: SearchStrategy[int] = st.integers(min_value=-1e6, max_value=1e6)
+# strategy for generating a random position between -1M and 1M
+st_ball_position: SearchStrategy[int] = st.integers(min_value=-1e6, max_value=1e6)
 
 # hypothesis strategy for generating random balls
-st_ball: SearchStrategy[Ball] = st.builds(Ball, x=ball_position,
-                                          y=ball_position, team=st.sampled_from(Team))
+st_ball: SearchStrategy[Ball] = st.builds(Ball, x=st_ball_position,
+                                          y=st_ball_position, team=st.sampled_from(Team))
 
 
 class TestBall(TestCase):
