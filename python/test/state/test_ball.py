0a7497a34b1963aea18ab01c37cdd81ba325deb9
David Abrahams <da.j.abrahams@gmail.com>
2019-08-31 13:04:39 -0700

    add readme and types

--- python/test/state/test_ball.py
+++ python/test/state/test_ball.py
@@ -1,21 +1,22 @@
 from unittest import TestCase
 from hypothesis import given
 import hypothesis.strategies as st
+from hypothesis.searchstrategy.strategies import SearchStrategy
 
 
 from state.ball import Ball, Team
 
 # some random position between -1M and 1M
-ball_position = st.integers(min_value=-1000000, max_value=1000000)
+ball_position: SearchStrategy[int] = st.integers(min_value=-1e6, max_value=1e6)
 
 # hypothesis strategy for generating random balls
-st_ball = st.builds(Ball, x=ball_position,
-                    y=ball_position, team=st.sampled_from(Team))
+st_ball: SearchStrategy[Ball] = st.builds(Ball, x=ball_position,
+                                          y=ball_position, team=st.sampled_from(Team))
 
 
 class TestBall(TestCase):
     @given(st_ball)
-    def test_draw_is_offset(self, ball):
+    def test_draw_is_offset(self, ball: Ball):
         rect = ball.get_shape()[2]
         self.assertEqual(rect.left, ball.x - ball.radius)
         self.assertEqual(rect.top, ball.y - ball.radius)
