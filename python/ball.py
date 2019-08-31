c9acdfe510b2d75bd21c0293128020d7da43e4e7
David Abrahams <da.j.abrahams@gmail.com>
2019-08-30 19:16:24 -0700

    move code into main

--- python/ball.py
+++ python/ball.py
@@ -1,10 +1,4 @@
-from typing import List
-
 class Ball:
     def __init__(self, x: int, y: int) -> None:
         self.x = x
         self.y = y
-
-l: List[int] = []
-ball = Ball(1, 2)
-l.append(ball.x)
