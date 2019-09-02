d187bd6349fd214f65845c1851d78a7b29fc7afe
Bill Warner <billwarner13@gmail.com>
2019-09-01 20:54:48 -0400

    PR feedback

--- python/strategies/myopic.py
+++ python/strategies/myopic.py
@@ -6,28 +6,27 @@ import state.heuristic_scores as score
 
 
 def best_keypresses(state: State, dt: int) -> Keys:
-    keypress_options = {
-        Keys(): 0.0,  # it's important that no action be first in case of uniform cost
-        Keys(left_pressed=True): 0.0,
-        Keys(right_pressed=True): 0.0,
-        Keys(up_pressed=True): 0.0,
-        Keys(down_pressed=True): 0.0,
-        Keys(left_pressed=True, up_pressed=True): 0.0,
-        Keys(right_pressed=True, up_pressed=True): 0.0,
-        Keys(left_pressed=True, down_pressed=True): 0.0,
-        Keys(right_pressed=True, down_pressed=True): 0.0
-    }
+    keypress_options = [
+        Keys()  # it's important that no action be first in case of uniform cost
+        Keys(left_pressed=True),
+        Keys(right_pressed=True),
+        Keys(up_pressed=True),
+        Keys(down_pressed=True),
+        Keys(left_pressed=True, up_pressed=True),
+        Keys(right_pressed=True, up_pressed=True),
+        Keys(left_pressed=True, down_pressed=True),
+        Keys(right_pressed=True, down_pressed=True)
+    ]
 
-    for keypress in keypress_options.keys():
-        keypress_options[keypress] = evaluate_keys(state, keypress, dt)
+    keypress_scores = [evaluate_keys(state, keypress, dt) for keypress in keypress_options]
 
-    return min(keypress_options.keys(), key=(lambda k: keypress_options[k]))
+    return min(zip(keypress_options, keypress_scores), key=(lambda tup: tup[1]))[0]
 
 
 def evaluate_keys(state: State, keypresses: Keys, dt: int) -> float:
     ego_ball = state.get_ego_ball()
     future_ball = ego_ball.simulate_input(keypresses)
-    future_ball.update(dt)  # todo this seems wrong
+    future_ball.update(dt)
     possible_world = copy.deepcopy(state)
     possible_world.balls[ego_ball.id] = future_ball
     return score.naive_ego_to_flag(possible_world)
